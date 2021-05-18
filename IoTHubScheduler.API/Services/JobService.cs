2e44bddb24b6c658edc16b5c47e8cfdaba058bb7
Jonathan Maes <jonathan.maes@niko.eu>
2021-05-18 14:55:46 +0200

    fix query issue no spaces allowed

--- IoTHubScheduler.API/Services/JobService.cs
+++ IoTHubScheduler.API/Services/JobService.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace IoTHubScheduler.API.Services
